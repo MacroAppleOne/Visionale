bb8c31769e1e88a3e4584d50108dbe5dd27e3a3a
RioJonathanWijaya <captainjj02@gmail.com>
2024-10-30 17:53:35 +0700

    fix(zoomButoon) zoom button size

--- Visionale/Views/Overlays/Grid/RuleofThirdsGrid.swift
+++ Visionale/Views/Overlays/Grid/RuleofThirdsGrid.swift
@@ -21,28 +21,28 @@ struct RuleOfThirdsGrid<CameraModel: Camera>: View {
                     path.move(to: CGPoint(x: width / 3, y: 0))
                     path.addLine(to: CGPoint(x: width / 3, y: height))
                 }
-                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [0, 2].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 2)
+                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [0, 2].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 1.5)
                 
                 // Right Vertical
                 Path { path in
                     path.move(to: CGPoint(x: (2 * width) / 3, y: 0))
                     path.addLine(to: CGPoint(x: (2 * width) / 3, y: height))
                 }
-                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [1, 3].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 2)
+                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [1, 3].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 1.5)
                 
                 // Upper Horizontal
                 Path { path in
                     path.move(to: CGPoint(x: 0, y: height / 3))
                     path.addLine(to: CGPoint(x: width, y: height / 3))
                 }
-                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [2, 3].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 2)
+                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [2, 3].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 1.5)
                 
                 // Lower Horizontal
                 Path { path in
                     path.move(to: CGPoint(x: 0, y: (2 * height) / 3))
                     path.addLine(to: CGPoint(x: width, y: (2 * height) / 3))
                 }
-                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [0, 1].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 2)
+                .stroke(((camera.mlcLayer?.guidanceSystem?.selectedKeypoints.contains{ [0, 1].contains($0) }) != nil) && camera.mlcLayer?.guidanceSystem?.isAligned ?? false ? Color.accent.opacity(0.7) : Color.white.opacity(0.7), lineWidth: 1.5)
             }
             
             
